fc205d0b1d99649e39727bc79eb7e3c7c6807377
Randy Steele <steelerx155@gmail.com>
2019-10-16 20:32:35 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+def line(queue)
+  queue = []
+  if queue = 0 
+    puts "The line is currently empty."
+  elsif
+  queue > 1 
+  puts "The line is currently: #{queue}."
+  queue.index(1)   
+end
