323fb2d9ca20d941993889afb85b1e39ade33eec
Randy Steele <steelerx155@gmail.com>
2019-10-16 20:55:58 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,16 @@
 # Write your code here.
-def line(queue)
-  queue = []
-  if queue = 0 
-    puts "The line is currently empty."
-  elsif
-  queue > 1 
-  puts "The line is currently: #{queue}."
-  queue.index(1)   
+require 'pry'
+def katz_deli []
+end
+
+def line(place_in_line)
+  place_in_line = []
+    name = ["Logan", "Avi", "Spencer"]
+  puts "The line is currently: #{name}."
+  if place_in_line = 0 
+    "The line is currently empty."
+    if place_in_line > 1 
+      puts "the line is currently: {name}"
+      place_in_line.collect.with_index(1) do |queue, index|
+        "The line is currently: #{index}., #{name}"
 end
